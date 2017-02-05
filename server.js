3a136e5ad80ed0de8e280edf127ab17c4b00b62c
Jagadeeshbuddha09 <jagadeeshbuddha09@gmail.com>
2017-02-05 15:29:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/articleone', function (req, res) {
+  res.send(path.join('Articleone page will be served shortly'));
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
