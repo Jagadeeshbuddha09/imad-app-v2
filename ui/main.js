964fb34917713b037ddf590b1fc493640e2aad76
Jagadeeshbuddha09 <jagadeeshbuddha09@gmail.com>
2017-02-11 07:49:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,63 @@
 console.log('Loaded!');
+
+var button = document.getElementById('counter');
+button.onclick=function(){
+   //Create an request object
+	var request = new XMLHttpRequest(); 
+	//capture the response and store it in a variable
+	console.log(request);
+	request.onreadystatechange = function(){
+		if(request.readyState === XMLHttpRequest.DONE)
+		{
+			//take some action
+			if(request.status == 200){
+				console.log(request.response.Text);
+				var counter = request.responseText;
+				var span=document.getElementById('count');
+				span.innerHTML = counter.toString();			
+			}
+		}
+		
+	}
+	//make the request
+	request.open('GET','http://localhost:8080/counter',true);
+	request.send(null);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var element = document.getElementById('maintext');
+//element.innerHTML = 'New Value';
+
+var img= document.getElementById('img1');
+var curmargin = 0;
+function moveRight(){
+  curmargin = curmargin + 10;
+  img1.style.marginLeft = curmargin+'px';
+}
+
+img1.onclick = function(){
+	  var interval = setInterval(moveRight,100);
+}*/
