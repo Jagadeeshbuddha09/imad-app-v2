c779059fc5c9db6391cb57dc39a2bdd3a671477e
Jagadeeshbuddha09 <jagadeeshbuddha09@gmail.com>
2017-02-11 07:55:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,7 +20,7 @@ button.onclick=function(){
 		
 	}
 	//make the request
-	request.open('GET','http://http://jagadeeshbuddha09.imad.hasura-app.io/counter',true);
+	request.open('GET','https://jagadeeshbuddha09.imad.hasura-app.io/counter',true);
 	request.send(null);
 };
 
