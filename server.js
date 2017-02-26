d8fdbe003e8465f61b39d8e4e90f10a8e67bdb03
Jagadeeshbuddha09 <jagadeeshbuddha09@gmail.com>
2017-02-26 20:38:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -116,7 +116,22 @@ app.get('/submit-name?:name',function(req,res){
 
 app.get('/:articleName', function (req, res) {
   var articleName=req.params.articleName;
-  res.send(CreateTemplate(articles[articleName]));
+  
+  var articleData = NULL;
+  pool.query('SELECT * FROM article WHERE title = article-one',function(err,result){
+    if(err){
+        res.status(500).send(err.toString());
+    }   
+    else{
+        if(result.rows.length === 0){
+            res.status(404).send('Article not found');
+        }
+        else{
+            articleData = result.rows[0];
+        }
+    }
+  });
+  res.send(CreateTemplate(articleData));
   //res.sendFile(path.join(__dirname,'ui','articleone.html'));
 });
 
