9eabadb1c972be88cab58b21b0ffb10efb6f76a9
Jagadeeshbuddha09 <jagadeeshbuddha09@gmail.com>
2017-02-26 22:09:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.get('/test-db', function(req,res){
 
 
 function CreateTemplate(data){
-    console.log(data);
+    
 	var title=data.title;
 	var date=data.date;
 	var content=data.content;
@@ -144,7 +144,7 @@ app.get('/:articleName', function (req, res) {
               res.status(404).send('Article not found');
           }
           else{
-              console.log("AM IN RIGHT PLACE");
+              console.log(result.rows);
               articleData = result.rows[0];
               //res.send(JSON.stringify(articleData));
               //console.log(articleData);
