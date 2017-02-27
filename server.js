ffe4c01a4bbf3629b1c0bc3e5d40d9071e7777e2
Jagadeeshbuddha09 <jagadeeshbuddha09@gmail.com>
2017-02-27 21:51:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -122,14 +122,16 @@ app.get('/:articleName', function (req, res) {
     var dummy = "dummy";
     pool.query('SELECT * FROM article WHERE id=1',function(err,result){
       if(err){
-          res.send(JSON.stringify(articleData));
+          
           res.status(500).send(err.toString());
              }   
       else{
+          res.send(JSON.stringify(articleData));
           if(result.rows.length === 0){
               res.status(404).send('Article not found');
           }
           else{
+              res.send(JSON.stringify(articleData));
               dummy = result.rows[0];
               //res.send(JSON.stringify(articleData));
               //console.log(articleData);
