877032052ad9642f7df6e1b097ce24f8f3b34678
Jagadeeshbuddha09 <jagadeeshbuddha09@gmail.com>
2017-03-12 21:59:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,35 +1,58 @@
 console.log('Loaded!');
 
-var button = document.getElementById('counter');
-button.onclick=function(){
+var submit = document.getElementById('submit_btn');
+submit.onclick=function(){
    //Create an request object
 	var request = new XMLHttpRequest(); 
 	//capture the response and store it in a variable
-	console.log(request);
+	var username = document.getElementById('username').value;
+	var password = document.getElementById('password').value;
+	
 	request.onreadystatechange = function(){
 		if(request.readyState === XMLHttpRequest.DONE)
 		{
 			//take some action
 			if(request.status == 200){
-				console.log(request.response.Text);
-				var counter = request.responseText;
-				var span=document.getElementById('count');
-				span.innerHTML = counter.toString();			
+				console.log(username);
+				console.log(password);
+				alert('userlogged in successfully');
 			}
 		}
-		
 	}
-	//make the request
-	request.open('GET','http://jagadeeshbuddha09.imad.hasura-app.io/counter',true);
-	request.send(null);
+	request.open('POST','http://jagadeeshbuddha09.imad.hasura-app.io/login',true);
+	request.header
+	request.send(JSON.stringify({'username':username,'password':password}));
+};	
+
+
+var submit_cmt=document.getElementById('submit_cmt');
+var comment=document.getElementById('comment');
+console.log('jagadeesh'+comment);
+submit_cmt.onclick=function()
+{
+	console.log(comment);
 };
 
 
-//send name to the server
 
 
 
-//render all the names(all names sent so far) returned by the server
+/*var element = document.getElementById('maintext');
+//element.innerHTML = 'New Value';
+
+var img= document.getElementById('img1');
+var curmargin = 0;
+function moveRight(){
+  curmargin = curmargin + 10;
+  img1.style.marginLeft = curmargin+'px';
+}
+
+img1.onclick = function(){
+	  var interval = setInterval(moveRight,100);
+}*/
+
+/*
+
 var submit = document.getElementById('submit_btn');
 submit.onclick=function(){
    //Create an request object
@@ -60,28 +83,29 @@ submit.onclick=function(){
 };	
 
 
-var submit_cmt=document.getElementById('submit_cmt');
-var comment=document.getElementById('comment');
-console.log('jagadeesh'+comment);
-submit_cmt.onclick=function()
-{
-	console.log(comment);
+
+
+var button = document.getElementById('counter');
+button.onclick=function(){
+   //Create an request object
+	var request = new XMLHttpRequest(); 
+	//capture the response and store it in a variable
+	console.log(request);
+	request.onreadystatechange = function(){
+		if(request.readyState === XMLHttpRequest.DONE)
+		{
+			//take some action
+			if(request.status == 200){
+				console.log(request.response.Text);
+				var counter = request.responseText;
+				var span=document.getElementById('count');
+				span.innerHTML = counter.toString();			
+			}
+		}
+		
+	}
+	//make the request
+	request.open('GET','http://jagadeeshbuddha09.imad.hasura-app.io/counter',true);
+	request.send(null);
 };
-
-
-
-
-
-/*var element = document.getElementById('maintext');
-//element.innerHTML = 'New Value';
-
-var img= document.getElementById('img1');
-var curmargin = 0;
-function moveRight(){
-  curmargin = curmargin + 10;
-  img1.style.marginLeft = curmargin+'px';
-}
-
-img1.onclick = function(){
-	  var interval = setInterval(moveRight,100);
-}*/
+*/
